d706e2a6e552c307a549508bb1c13974071b7e69
SVRourke <samuelvanrourke@gmail.com>
2021-04-14 13:02:44 -0400

    stubbed out routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import {
+  Home, 
+  Contact, 
+  Distributor 
+} from './pages'
 
 function App() {
   return (
-    <h1>APP</h1>
+    <div>
+      <h1>APP</h1>
+      <Link to='/'>home</Link>
+      <Link to='/partner'>Become a distributor</Link>
+      <Link to='/contact'>Contact</Link>
+
+    <Switch>
+
+      <Route exact path='/'>
+        <Home />
+      </Route>
+
+      <Route path='/partner' >
+        <Distributor />
+      </Route>
+
+      <Route path='/contact' >
+        <Contact />
+      </Route>
+
+    </Switch>
+
+    </div>
   );
 }
 
