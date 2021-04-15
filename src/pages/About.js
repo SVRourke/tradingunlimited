874289bb9b9c9de8db9022aff02ade262f228786
SVRourke <samuelvanrourke@gmail.com>
2021-04-15 12:33:49 -0400

    created about page

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+// ALERT Maybe Unnecessary
+import React from 'react'
+
+import { Header } from '../blocks'
+
+export default function About() {
+    return (
+        <div className='main' >
+            <Header title={'About'} />
+        
+            <p>ABOUT</p>
+        </div>
+    )
+}
