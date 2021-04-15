7c6512344bf9987afcf0e3fc8c86d0364706226b
SVRourke <samuelvanrourke@gmail.com>
2021-04-15 12:33:34 -0400

    bulk export from blocks

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,3 +1,17 @@
 import Header from './Header'
+import Footer from './Footer'
+import StatsBlock from './StatsBlock'
+import AboutBlock from './AboutBlock'
+import CategoriesBlock from './CategoriesBlock'
+import PartnerCtaBlock from './PartnerCtaBlock'
+import OnPageContactBlock from './OnPageContactBlock'
 
-export { Header }
+export { 
+    Header,
+    Footer,
+    StatsBlock, 
+    AboutBlock, 
+    CategoriesBlock,
+    PartnerCtaBlock,
+    OnPageContactBlock
+ }
