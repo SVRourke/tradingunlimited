7a6c67f9fdbe12d6171d25970c79fe561e189044
SVRourke <samuelvanrourke@gmail.com>
2021-04-15 12:33:59 -0400

    added about page to bulk export

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from './Home'
 import Contact from './Contact'
 import Distributor from './Distributor'
+import About from './About'
 
-export {Home, Contact, Distributor}
+export {Home, Contact, Distributor, About}
