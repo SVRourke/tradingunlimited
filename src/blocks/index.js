93851631e7cc1a4f988052dfaa9de6cbfcd2b735
SVRourke <samuelvanrourke@gmail.com>
2021-04-19 11:42:57 -0400

    added homeheader

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,4 +1,5 @@
 import Header from './Header'
+import HomeHeader from './HomeHeader'
 import Footer from './Footer'
 import StatsBlock from './StatsBlock'
 import AboutBlock from './AboutBlock'
@@ -7,6 +8,7 @@ import PartnerCtaBlock from './PartnerCtaBlock'
 import OnPageContactBlock from './OnPageContactBlock'
 
 export { 
+    HomeHeader,
     Header,
     Footer,
     StatsBlock, 
