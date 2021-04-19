2544eb4f0970d5d7d85eecfc69d057314e614c89
SVRourke <samuelvanrourke@gmail.com>
2021-04-19 17:15:49 -0400

    stubeed out elements for stats block

--- src/blocks/StatsBlock.js
+++ src/blocks/StatsBlock.js
@@ -1,10 +1,47 @@
 import React from 'react'
 
+const statsRow = {
+  
+}
 export default function StatsBlock() {
     return (
         <div className={'layout-block'} >
-          <div style={{height: '50vh', display: 'grid', placeItems: 'center', backgroundColor: 'rgba(0,0,0,0.2', margin: '1rem 0 1rem 0'}}>
-            <p>STATS</p>
+          <div style={{height: '50vh'}}>
+            <div className="row" style={statsRow}>
+              <div className="stat-block">
+                <h3>Location:</h3>
+                <p>Hollywood FL, USA</p>
+              </div>
+
+              <div className="stat-block">
+                <h3>Call Us:</h3>
+                <p>+1 954-783-0400</p>
+              </div>
+
+              <div className="stat-block">
+                <h3>Email:</h3>
+                <p>send us a message!</p>
+              </div>
+            </div>
+
+            <div className="row" >
+              <div className="stat-block">
+                <h3>Speed:</h3>
+                <p>Orders Shipped Daily</p>
+              </div>
+
+              <div className="stat-block">
+                <h3>Flexibility:</h3>
+                <p>Multi-Channel</p>
+              </div>
+
+              <div className="stat-block">
+                <h3>Volume:</h3>
+                <p>Multi-Warehouse</p>
+              </div>
+            </div>
+
+
           </div>
         </div>
     )
