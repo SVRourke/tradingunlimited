fdb0a7cd48fb22a62a79b693d6322a57b9c35751
SVRourke <samuelvanrourke@gmail.com>
2021-04-26 10:41:55 -0400

    imported font awesome icons, added them to stats blocks

--- src/blocks/StatsBlock.js
+++ src/blocks/StatsBlock.js
@@ -9,16 +9,19 @@ export default function StatsBlock() {
           <div style={{height: '50vh'}}>
             <div className="row" style={statsRow}>
               <div className="stat-block">
+                <i class="fa fa-map-marker"></i>
                 <h3>Location:</h3>
                 <p>Hollywood FL, USA</p>
               </div>
 
               <div className="stat-block">
+                <i class="fa fa-mobile "></i>
                 <h3>Call Us:</h3>
                 <p>+1 954-783-0400</p>
               </div>
 
               <div className="stat-block">
+                <i class="fa fa-paper-plane"></i>
                 <h3>Email:</h3>
                 <p>send us a message!</p>
               </div>
@@ -26,16 +29,19 @@ export default function StatsBlock() {
 
             <div className="row" >
               <div className="stat-block">
+                <i class="fa fa-bolt"></i>
                 <h3>Speed:</h3>
                 <p>Orders Shipped Daily</p>
               </div>
 
               <div className="stat-block">
+                <i class="fa fa-sitemap"></i>
                 <h3>Flexibility:</h3>
                 <p>Multi-Channel</p>
               </div>
 
               <div className="stat-block">
+                <i class="fa fa-cubes"></i>
                 <h3>Volume:</h3>
                 <p>Multi-Warehouse</p>
               </div>
