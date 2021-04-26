8c634d0c437a33b41ee132f25d814e518d1c7ee8
SVRourke <samuelvanrourke@gmail.com>
2021-04-26 12:31:46 -0400

    updated css import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(
