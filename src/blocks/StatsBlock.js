f3988fa7d8a2dbccdacbe2018ee9aebd384ecd06
SVRourke <samuelvanrourke@gmail.com>
2021-04-26 12:32:10 -0400

    added font awesome icons, styled blocks

--- src/blocks/StatsBlock.js
+++ src/blocks/StatsBlock.js
@@ -1,49 +1,71 @@
 import React from 'react'
+import '../styles/stats-block.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMap } from '@fortawesome/free-regular-svg-icons'
+import {   
+  faMapMarkerAlt,
+  faPhoneAlt,
+  faEnvelopeOpenText,
+  faStopwatch,
+  faSitemap,
+  faCubes
+ } from '@fortawesome/free-solid-svg-icons'
 
-const statsRow = {
-  
-}
+
 export default function StatsBlock() {
     return (
         <div className={'layout-block'} >
           <div style={{height: '50vh'}}>
-            <div className="row" style={statsRow}>
+
+            <div className="row" style={{justifyContent: 'space-between'}}>
               <div className="stat-block">
-                <i class="fa fa-map-marker"></i>
-                <h3>Location:</h3>
-                <p>Hollywood FL, USA</p>
+                <FontAwesomeIcon icon={faMapMarkerAlt} size ="4x" />
+                <div className="stat-sub-block">
+                  <h3>Location:</h3>
+                  <p>Hollywood FL, USA</p>
+                </div>
               </div>
 
               <div className="stat-block">
-                <i class="fa fa-mobile "></i>
-                <h3>Call Us:</h3>
-                <p>+1 954-783-0400</p>
+                <FontAwesomeIcon icon={faPhoneAlt} size ="4x" />
+                <div className="stat-sub-block">
+                  <h3>Call Us:</h3>
+                  <p>+1 954-783-0400</p>
+                </div>
               </div>
 
               <div className="stat-block">
-                <i class="fa fa-paper-plane"></i>
-                <h3>Email:</h3>
-                <p>send us a message!</p>
+                <FontAwesomeIcon icon={faEnvelopeOpenText} size ="4x" />
+                <div className="stat-sub-block">
+                  <h3>Email:</h3>
+                  <p>send us a message!</p>
+                </div>
               </div>
             </div>
 
             <div className="row" >
               <div className="stat-block">
-                <i class="fa fa-bolt"></i>
-                <h3>Speed:</h3>
-                <p>Orders Shipped Daily</p>
+                <FontAwesomeIcon icon={faStopwatch} size ="4x" />
+                <div className="stat-sub-block">
+                  <h3>Speed:</h3>
+                  <p>Orders Shipped Daily</p>
+                </div>
               </div>
 
               <div className="stat-block">
-                <i class="fa fa-sitemap"></i>
-                <h3>Flexibility:</h3>
-                <p>Multi-Channel</p>
+                <FontAwesomeIcon icon={faSitemap} size ="4x" />
+                <div className="stat-sub-block">
+                  <h3>Flexibility:</h3>
+                  <p>Multi-Channel</p>
+                </div>
               </div>
 
               <div className="stat-block">
-                <i class="fa fa-cubes"></i>
-                <h3>Volume:</h3>
-                <p>Multi-Warehouse</p>
+                <FontAwesomeIcon icon={faCubes} size ="4x" />
+                <div className="stat-sub-block">
+                  <h3>Volume:</h3>
+                  <p>Multi-Warehouse</p>
+                </div>
               </div>
             </div>
 
