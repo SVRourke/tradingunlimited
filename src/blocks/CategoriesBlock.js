0dd480ef961e086e32acc4fef5e2ef445ce7228c
SVRourke <samuelvanrourke@gmail.com>
2021-04-27 17:05:30 -0400

    added content to categories block

--- src/blocks/CategoriesBlock.js
+++ src/blocks/CategoriesBlock.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import '../styles/categories-block.css'
 
 export default function CategoriesBlock() {
     return (
         <div className={'layout-block'} >
-          <div style={{height: '100vh', display: 'grid', placeItems: 'center', backgroundColor: 'rgba(0,0,0,0.2', margin: '1rem 0 1rem 0'}}>
-            <p>Categories</p>
+          <div className="categories_content">
+            <div className="categories_heading">
+              <h2>Our Products</h2>
+              <p>We're always adding new products to our inventory</p>
+            </div>
+
+            <div className="categories_cards">
+              <div className='row'>
+                <div className='category_card'>
+                  <h3>Video</h3>
+                </div>
+
+                <div className='category_card'>
+                  <h3>Audio</h3>
+                </div>
+
+                <div className='category_card'>
+                  <h3>Cameras & Lenses</h3>
+                </div>
+              </div>
+
+              <div className='row'>
+                <div className='category_card'>
+                  <h3>Personal Electronics</h3>
+                </div>
+
+                <div className='category_card'>
+                  <h3>Drones</h3>                  
+                </div>
+
+                <div className='category_card'>
+                  <h3>Computers & IT Equipment</h3>
+                </div>
+              </div>
+
+            </div>
+
+            
           </div>
         </div>
     )
