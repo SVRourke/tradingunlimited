5a16d0e8fb9bbf898f1ec55cf672301ff7e46a7f
SVRourke <samuelvanrourke@gmail.com>
2021-05-12 17:32:22 -0400

    stubbed out about section (needs to be broken into 3 separate

---
+++ src/blocks/About/About.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './about.scss'
+
+export default function AboutBlock() {
+    return (
+        <div className={'layout-block about-layout-block'} >
+          <div className="about-content row">
+            <div className="left-text">
+              <h2>
+                17 Years of <span>Experience</span> in Distribution
+              </h2>
+
+              <p>
+                Trading Unlimited, LLC. (2004) Leading distributor of brand name
+                photographic equipment and consumer electronics. From Digital 
+                cameras, Action cameras, Camcorders, Lenses and accessories, to 
+                Laptop computers, Tablets, Cellular phones, TV’s, LCD monitors, 
+                DVD Players, Home Theater, Projectors, Portable audio and mobile 
+                electronics, Sound Bars, Drones, UPS and Voltage Regulators, 
+                Multimedia flash and storage devices, we are here to provide you 
+                with the best solution at the best possible price. We carry a wide 
+                range of products to fit the needs of your growing business. 
+              </p>
+
+              <p>
+                Our valued customers are our best partners and hence treated as 
+                such!
+              </p>
+
+              <p>
+                Trading Unlimited distributes brand name products, like Samsung,
+                Sharp, LG, Nikon, Canon, Ricoh, Pentax, Pioneer, Hisense, Panasonic,
+                DJI, SanDisk, Yuneec, JVC, HP, Leica, Smartbitt, Lexar among others,
+                and is always adding new lines to our stock, to help our customers 
+                to grow in their markets.
+              </p>
+
+              <p>
+                We have 3 warehouses located in Hollywood, Miami and El Doral, 
+                Florida. All our merchandise is always in stock for immediate 
+                delivery.
+              </p>
+
+              <p>
+                Our great buying power as well as our close relation with the top 
+                manufacturers of the industry, often allows us to offer unique deals 
+                at unbeatable prices. Contact a sales associate today to see what we
+                can offer you and how can we help expand your business.
+              </p>
+
+            </div>
+
+            <div className="right-image" >
+              <div>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
