9c69619e8d3a2a7ebdabef22167f71abe14431b1
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 16:57:20 -0400

    added placeholer paragraph

--- src/blocks/About.js
+++ src/blocks/About.js
@@ -9,6 +9,13 @@ export default function About() {
                 <div className={'column'}>
                     <p className={'blue-tab'}>About Us</p>
                     <h2 className={'big-title'}>17 Years of <span className={'blue-text'}>Experience</span></h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat auctor 
+                        cursus. Phasellus est ex, vehicula non ultricies at, pretium ac lacus. Sed 
+                        luctus nibh eu dui rhoncus pretium. In dui sapien, faucibus nec porttitor non,
+                        pretium ut est. Sed nisi felis, luctus quis molestie at, blandit ut velit. 
+                        Aenean sit amet efficitur diam.
+                    </p>
 
                 </div>
 
