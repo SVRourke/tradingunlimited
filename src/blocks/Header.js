9f9768a99c7125ca15775413ece20beb9490bff6
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 14:44:06 -0400

    stubbed out content structure

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+import Logo from '../assets/top-logo.png'
+import Words from '../assets/trading-words.png'
 export default function Header() {
     return (
         <div className={'block_full-screen'}>
-            Header
+            <section>
+                <img src={Logo} />
+                <nav>
+                    <a href="#">home</a>
+                    <a href="#">about</a>
+                    <a href="#">products</a>
+                    <a href="#">contact</a>
+                </nav>
+            </section>
+
+            <section>
+                <img src={Words} />
+                
+                <div>
+                    <button>Learn More</button>
+                    <button>Partner With Us</button>
+                </div>
+            </section>
+
+            <section>
+                <p>call 1+ 305-777-XXXX</p>
+            </section>
         </div>
     )
 }
