135e845da8c6d913ab083a6ea0db830f4d8be680
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 15:53:08 -0400

    format, add class to content section

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -6,24 +6,30 @@ import '../styles/Header.scss'
 export default function Header() {
     return (
         <header className={'block_full-screen'}>
+
             <section className={'navbar'}>
                 <img src={Logo} />
+                
                 <nav>
                     <a href="#">home</a>
                     <a href="#">about</a>
                     <a href="#">products</a>
                     <a href="#">contact</a>
+                
                 </nav>
             </section>
 
-            <section>
+
+            <section className={'header_content'}>
                 <img src={Words} />
                 
                 <div>
                     <button>Learn More</button>
                     <button>Partner With Us</button>
+
                 </div>
             </section>
+
 
             <section>
                 <p>call 1+ 305-777-XXXX</p>
