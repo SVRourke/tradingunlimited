8c1bc51f15c5fda7b44b2e0f53aeef5f959b554b
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 16:43:38 -0400

    markdown structure preliminary

--- src/blocks/About.js
+++ src/blocks/About.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import '../styles/About.scss'
+
 export default function About() {
     return (
-        <div className={'block_full-screen'}>
-            About
-            
-        </div>
+        <section id='about' className={'block_full-screen about-block'}>
+            <section className={'row'} >
+                <div className={'column'}>
+                    <p className={'blue-tab'}>About Us</p>
+                    <h2 className={'big-title'}>17 Years of <span className={'blue-text'}>Experience</span></h2>
+
+                </div>
+
+                <div className={'column'}>
+
+                </div>
+
+            </section>
+        </section>
     )
 }
