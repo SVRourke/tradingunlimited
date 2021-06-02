b01d2e22994c2fe593461447c963d93fa8b739f2
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 17:21:46 -0400

    import styles, added some placeholder text

--- src/blocks/Brands.js
+++ src/blocks/Brands.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../styles/Brands.scss'
 
 export default function Brands() {
     return (
         <section id='brands' className={'block'}>
-            <h2 className={'big-title'}>Our Partners</h2>            
+            <h2 className={'big-title'}>Our Partners</h2>
+            <p>
+                rhoncus pretium. In dui sapien, faucibus nec porttitor non, pretium ut est. Sed 
+                nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur 
+                diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant 
+                morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce 
+                gravida feugiat efficitur. Nulla consequat tincidunt nisi sed viverra.
+            </p>            
         </section>
     )
 }
