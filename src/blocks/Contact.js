4acaf82f92ba341091186a54338d922d6d56d919
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 11:08:33 -0400

    stub contact block

---
+++ src/blocks/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <div className={'block_full-screen'}>
+            Contact            
+        </div>
+    )
+}
