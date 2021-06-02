89573e7ccb48904b83174158056771fecdccc954
SVRourke <samuelvanrourke@gmail.com>
2021-06-02 14:26:26 -0400

    add classname for block container

--- src/blocks/Footer.js
+++ src/blocks/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div classname={'block'}>
+        <div className={'block'}>
             Footer
         </div>
     )
