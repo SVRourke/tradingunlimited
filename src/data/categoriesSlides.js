9a89b3726c2faa9b17ad956aa471db5a66680020
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 10:13:58 -0400

    created data for categoried slides

---
+++ src/data/categoriesSlides.js
@@ -0,0 +1,38 @@
+export const slides = [
+    {
+        tab: 'Video',
+        title: '',
+        blurb: '',
+        img: ''
+    },
+    {
+        tab: 'Audio',
+        title: '',
+        blurb: '',
+        img: ''
+    },
+    {
+        tab: 'Photography',
+        title: '',
+        blurb: '',
+        img: ''
+    },
+    {
+        tab: 'Personal Electronics',
+        title: '',
+        blurb: '',
+        img: ''
+    },
+    {
+        tab: 'Drones',
+        title: '',
+        blurb: '',
+        img: ''
+    },
+    {
+        tab: 'Computers and I.T. Equipment',
+        title: '',
+        blurb: '',
+        img: ''
+    },
+]
