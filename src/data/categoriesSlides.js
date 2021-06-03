8af2552cc31202b90db19f9b943dbb9f272ebb5b
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 10:57:20 -0400

    stubbed out data

--- src/data/categoriesSlides.js
+++ src/data/categoriesSlides.js
@@ -1,38 +1,38 @@
 export const slides = [
     {
         tab: 'Video',
-        title: '',
-        blurb: '',
+        title: 'OLED, QLED, 4K & 8K',
+        blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: ''
     },
     {
         tab: 'Audio',
-        title: '',
-        blurb: '',
+        title: 'Speakers, Subwoofers & Soundbars',
+        blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: ''
     },
     {
         tab: 'Photography',
-        title: '',
-        blurb: '',
+        title: 'Cameras, Lenses & Accessories',
+        blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: ''
     },
     {
         tab: 'Personal Electronics',
-        title: '',
-        blurb: '',
+        title: 'Gaming, Cellphones & Tablets',
+        blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: ''
     },
     {
         tab: 'Drones',
-        title: '',
-        blurb: '',
+        title: 'Sed nisi felis, luctus quis molestie at',
+        blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: ''
     },
     {
         tab: 'Computers and I.T. Equipment',
-        title: '',
-        blurb: '',
+        title: 'Computers, Laptops & I.T. Equipment',
+        blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: ''
     },
 ]
