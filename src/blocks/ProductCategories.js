85ea664c862d6582f611a04e9a6494a475c98b89
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 10:55:54 -0400

    create functions to handle changing the active slide

--- src/blocks/ProductCategories.js
+++ src/blocks/ProductCategories.js
@@ -15,6 +15,14 @@ export default function ProductCategories() {
     
     let slide = slides[slideIdx]
 
+    const nextSlide = () => {
+        slideIdx < slides.length - 1 ? setSlide(slideIdx + 1) : setSlide(0)
+    }
+
+    const prevSlide = () => {
+        slideIdx > 0 ? setSlide(slideIdx - 1) : setSlide(slides.length - 1)
+    }
+
     return (
         <div className={'block_full-screen'}>
             <p className={'blue-tab'}>{slide.tab}</p>
