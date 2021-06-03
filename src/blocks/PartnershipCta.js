6a35937fbdc3694663ca9434cb5d71c776196b19
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 16:48:07 -0400

    stub partner block content/structure

--- src/blocks/PartnershipCta.js
+++ src/blocks/PartnershipCta.js
@@ -3,7 +3,19 @@ import React from 'react'
 export default function PartnershipCta() {
     return (
         <section className={'block'}>
-            Become a partner            
+            <h2 className={'big-title'}>Become a Partner</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat 
+                auctor cursus. Phasellus est ex, vehicula non ultricies at, pretium 
+                ac lacus. Sed luctus nibh eu dui rhoncus pretium. In dui sapien, 
+                faucibus nec porttitor non, pretium ut est. Sed nisi felis, luctus 
+                quis molestie at, blandit ut velit. Aenean sit amet efficitur diam.
+            </p>
+            
+            <div className={'row'} >
+                <button>Download an Application</button>
+                <button>Contact Us</button>
+            </div>
         </section>
     )
 }
