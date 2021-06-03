d544154187767840f74a8be24eae31619b650a6b
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 12:30:53 -0400

    switched block to section from div, created 2 rows 1 for slide 1 for controls

--- src/blocks/ProductCategories.js
+++ src/blocks/ProductCategories.js
@@ -24,13 +24,20 @@ export default function ProductCategories() {
     }
 
     return (
-        <div className={'block_full-screen'}>
-            <p className={'blue-tab'}>{slide.tab}</p>
-            <h2 className={'big-title'}>{slide.title}</h2>
-            <p>{slide.blurb}</p>
-            <button onClick={nextSlide} >next</button>
-            <button onClick={prevSlide} >previous</button>
+        <section className={'block_full-screen'}>
+            <div className={'row'}>
 
-        </div>
+                <p className={'blue-tab'}>{slide.tab}</p>
+                <h2 className={'big-title'}>{slide.title}</h2>
+                <p>{slide.blurb}</p>
+
+            </div>
+
+            <div className={'row'}>
+                <button onClick={nextSlide} >next</button>
+                <button onClick={prevSlide} >previous</button>
+            </div>
+
+        </section>
     )
 }
