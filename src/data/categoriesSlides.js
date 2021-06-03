b934dcb6257acf1d51e30dfe7dcf6cb721fc52d5
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 14:22:14 -0400

    added placeholder image

--- src/data/categoriesSlides.js
+++ src/data/categoriesSlides.js
@@ -3,36 +3,36 @@ export const slides = [
         tab: 'Video',
         title: 'OLED, QLED, 4K & 8K',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-        img: ''
+        img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Audio',
         title: 'Speakers, Subwoofers & Soundbars',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-        img: ''
+        img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Photography',
         title: 'Cameras, Lenses & Accessories',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-        img: ''
+        img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Personal Electronics',
         title: 'Gaming, Cellphones & Tablets',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-        img: ''
+        img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Drones',
         title: 'Sed nisi felis, luctus quis molestie at',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-        img: ''
+        img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Computers and I.T. Equipment',
         title: 'Computers, Laptops & I.T. Equipment',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-        img: ''
+        img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
 ]
