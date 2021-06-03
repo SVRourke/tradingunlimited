858da01067e689bd78c33d2c0a82470c598d2dc4
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 17:16:26 -0400

    reduce placeholder text

--- src/blocks/PartnershipCta.js
+++ src/blocks/PartnershipCta.js
@@ -8,9 +8,7 @@ export default function PartnershipCta() {
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat 
                 auctor cursus. Phasellus est ex, vehicula non ultricies at, pretium 
-                ac lacus. Sed luctus nibh eu dui rhoncus pretium. In dui sapien, 
-                faucibus nec porttitor non, pretium ut est. Sed nisi felis, luctus 
-                quis molestie at, blandit ut velit. Aenean sit amet efficitur diam.
+                ac lacus. 
             </p>
             
             <div className={'row'} >
