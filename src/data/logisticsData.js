75927d3c7529cc8a773dca94d8f6b9efd5a6a2b4
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 16:34:21 -0400

    created logistics data

---
+++ src/data/logisticsData.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        tab: 'Flexible',
+        title: 'Multi-Location',
+        content: 'Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+    {
+        tab: 'Distribution',
+        title: 'Multi-Channel',
+        content: 'Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+    {
+        tab: 'Speed',
+        title: 'Same Day Shipping',
+        content: 'Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+    {
+        tab: 'Support',
+        title: 'Customer Service',
+        content: 'Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+    {
+        tab: 'Delivery',
+        title: 'Frieght Partners',
+        content: 'Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+    {
+        tab: 'Flexible',
+        title: 'Multi-Location',
+        content: 'Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+    },
+]
