2c489c1f7dca574dbd4e7421e68302f0810cd18b
SVRourke <samuelvanrourke@gmail.com>
2021-06-03 10:07:38 -0400

    import useState, and icons, stub useState

--- src/blocks/ProductCategories.js
+++ src/blocks/ProductCategories.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {
+    useState
+} from 'react'
+
+import { 
+    BsPhone,
+    BsEnvelopeOpen,
+    BsShieldLock
+ } from "react-icons/bs";
+
 
 export default function ProductCategories() {
+    const [slide, setSlide ] = useState(slides)
     return (
         <div className={'block_full-screen'}>
-            Categories            
+            Categories
+            <BsPhone />
+            <BsEnvelopeOpen />
+            <BsShieldLock />            
         </div>
     )
 }
