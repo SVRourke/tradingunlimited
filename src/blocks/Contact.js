8a37d610f5d18dbbdf400c8cc1fb693af0c26954
SVRourke <samuelvanrourke@gmail.com>
2021-06-04 09:49:10 -0400

    add placeholder content

--- src/blocks/Contact.js
+++ src/blocks/Contact.js
@@ -5,7 +5,16 @@ import '../styles/Contact.scss'
 export default function Contact() {
     return (
         <section id="contact" className={'block_full-screen'}>
-            <h2 className={'big-title'}>Contact Us</h2>             
+            <h2 className={'big-title'}>Contact Us</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut 
+                placerat auctor cursus. Phasellus est ex, vehicula non ultricies 
+                at, pretium ac lacus. Sed luctus nibh eu dui rhoncus pretium. 
+                In dui sapien, faucibus nec porttitor non, pretium ut est. Sed 
+                nisi felis, luctus quis molestie at, blandit ut velit. Aenean 
+                sit amet efficitur diam.
+            </p>
+                         
         </section>
     )
 }
