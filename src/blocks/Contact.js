2ad4588d58a451981be5c6f197016b3666ec2fad
SVRourke <samuelvanrourke@gmail.com>
2021-06-04 09:52:53 -0400

    use utility classes to rough out layout

--- src/blocks/Contact.js
+++ src/blocks/Contact.js
@@ -5,16 +5,31 @@ import '../styles/Contact.scss'
 export default function Contact() {
     return (
         <section id="contact" className={'block_full-screen'}>
-            <h2 className={'big-title'}>Contact Us</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut 
-                placerat auctor cursus. Phasellus est ex, vehicula non ultricies 
-                at, pretium ac lacus. Sed luctus nibh eu dui rhoncus pretium. 
-                In dui sapien, faucibus nec porttitor non, pretium ut est. Sed 
-                nisi felis, luctus quis molestie at, blandit ut velit. Aenean 
-                sit amet efficitur diam.
-            </p>
-                         
+            <div className={'row'} >
+                <div className={'column'}>
+                    <h2 className={'big-title'}>Contact Us</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut 
+                        placerat auctor cursus. Phasellus est ex, vehicula non ultricies 
+                        at, pretium ac lacus. Sed luctus nibh eu dui rhoncus pretium. 
+                        In dui sapien, faucibus nec porttitor non, pretium ut est. Sed 
+                        nisi felis, luctus quis molestie at, blandit ut velit. Aenean 
+                        sit amet efficitur diam.
+                    </p>
+                </div>
+                
+                <div className={'column'}>
+                    <form>
+                        <label for="name">Name:</label>
+                        <input type='text' />
+                        
+                        <label for="email">Name:</label>
+                        <input type='email' />
+
+                        <input type='submit' value='send' />
+                    </form>
+                </div>
+            </div>
         </section>
     )
 }
