2770f166b66c51eca7a46fbd5d16738b162fbfd5
SVRourke <samuelvanrourke@gmail.com>
2021-06-07 16:06:07 -0400

    added svg separator

--- src/blocks/PartnershipCta.js
+++ src/blocks/PartnershipCta.js
@@ -1,36 +1,34 @@
 import React from "react";
-import SeparatorTop from "../assets/separatortop.svg";
 import "../styles/PartnershipCta.scss";
 
 export default function PartnershipCta() {
   return (
     <section id="partnerCta" className={"block"}>
-      <div className={"svgContainer"}>
-        <svg
-          id="separatorTop"
-          width="301.13034mm"
-          height="72.314789mm"
-          viewBox="0 0 301.13034 72.314789"
-        >
-          <path
-            fill='white'
-            id="rect31"
-            d="M 43.375903,83.509455 186.1829,82.868406 185.86238,105.75535 c -30.93061,-16.026221 -113.63928,20.19304 -142.646739,0 z"
-          />
-        </svg>
+      <svg viewBox="0 0 800 59">
+        <g transform="matrix(1,0,0,1,6.57252e-14,-329)">
+          <g transform="matrix(1.00766,0,0,1.08407,-2.46102,-27.6597)">
+            <path
+              d="M796.36,329.001L2.442,329.001L2.442,357.597C398.644,445.28 398.412,271.416 796.36,357.597L796.36,329.001Z"
+            />
+          </g>
+        </g>
+      </svg>
+
+      <div className={'partner-content'}>
+        <h2 className={"big-title"}>Become a Partner</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat
+          auctor cursus. Phasellus est ex, vehicula non ultricies at, pretium ac
+          lacus.
+        </p>
+
+        <div className={"row"}>
+          <button>Download an Application</button>
+          <button>Contact Us</button>
+        </div>
+
       </div>
 
-      <h2 className={"big-title"}>Become a Partner</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat
-        auctor cursus. Phasellus est ex, vehicula non ultricies at, pretium ac
-        lacus.
-      </p>
-
-      <div className={"row"}>
-        <button>Download an Application</button>
-        <button>Contact Us</button>
-      </div>
     </section>
   );
 }
