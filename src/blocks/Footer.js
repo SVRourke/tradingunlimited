6aa61c676c7c9eca391618f5945a8ffe8a597c9c
SVRourke <samuelvanrourke@gmail.com>
2021-06-07 16:13:56 -0400

    added logo

--- src/blocks/Footer.js
+++ src/blocks/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import topLogo from '../assets/top-logo.png'
 
 export default function Footer() {
     return (
         <section className={'block'}>
-            Footer
+            <img src={topLogo} />
         </section>
     )
 }
