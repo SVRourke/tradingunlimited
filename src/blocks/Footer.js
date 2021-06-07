a82d785ca6bd30938ad42f7eb2238302769a0f7e
SVRourke <samuelvanrourke@gmail.com>
2021-06-07 16:16:11 -0400

    stub out footer info

--- src/blocks/Footer.js
+++ src/blocks/Footer.js
@@ -5,6 +5,24 @@ export default function Footer() {
     return (
         <section className={'block'}>
             <img src={topLogo} />
+
+            <section className={'footer-nav'} >
+                <nav>
+                    <a>Home</a>
+                    <a>About</a>
+                    <a>Products</a>
+                </nav>
+                <nav>
+                    <a>Logistics</a>
+                    <a>Become A Distributor</a>
+                    <a>Contact</a>
+                </nav>
+                <div>
+                    {/* Location badge */}
+                    {/* Phone badge */}
+                    {/* Message badge */}
+                </div>
+            </section>
         </section>
     )
 }
