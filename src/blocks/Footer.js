2ad9362c1edeefe505a7ef1edc10ebf11ceae963
SVRourke <samuelvanrourke@gmail.com>
2021-06-08 10:21:17 -0400

    import footer stylesheet, format

--- src/blocks/Footer.js
+++ src/blocks/Footer.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import topLogo from '../assets/top-logo.png'
+import '../styles/Footer.scss'
 
 export default function Footer() {
     return (
-        <footer className={'block'}>
+        <footer id='footer' className={'block'}>
             <img src={topLogo} />
+            
+            <nav>
+                <a>Home</a>
+                <a>About</a>
+                <a>Products</a>
+            </nav>
+            
+            <nav>
+                <a>Logistics</a>
+                <a>Become A Distributor</a>
+                <a>Contact</a>
+            </nav>
+            
+            <div>
+                {/* Location badge */}
+                {/* Phone badge */}
+                {/* Message badge */}
+            </div>
 
-            <section className={'footer-nav'} >
-                <nav>
-                    <a>Home</a>
-                    <a>About</a>
-                    <a>Products</a>
-                </nav>
-                <nav>
-                    <a>Logistics</a>
-                    <a>Become A Distributor</a>
-                    <a>Contact</a>
-                </nav>
-                <div>
-                    {/* Location badge */}
-                    {/* Phone badge */}
-                    {/* Message badge */}
-                </div>
-            </section>
         </footer>
     )
 }
