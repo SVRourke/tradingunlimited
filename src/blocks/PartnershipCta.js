95aed2bf53ea0f67568e0e8c2315692b951649f7
SVRourke <samuelvanrourke@gmail.com>
2021-06-14 14:50:15 -0400

    refactor using separate separator file

--- src/blocks/PartnershipCta.js
+++ src/blocks/PartnershipCta.js
@@ -1,20 +1,13 @@
 import React from "react";
 import "../styles/PartnershipCta.scss";
+import { ReactComponent as Separator } from "../assets/separator.svg";
 
 export default function PartnershipCta() {
   return (
     <section id="partnerCta" className={"block"}>
-      <svg viewBox="0 0 800 59">
-        <g transform="matrix(1,0,0,1,6.57252e-14,-329)">
-          <g transform="matrix(1.00766,0,0,1.08407,-2.46102,-27.6597)">
-            <path
-              d="M796.36,329.001L2.442,329.001L2.442,357.597C398.644,445.28 398.412,271.416 796.36,357.597L796.36,329.001Z"
-            />
-          </g>
-        </g>
-      </svg>
+      <Separator />
 
-      <div className={'partner-content'}>
+      <div className={"partner-content"}>
         <h2 className={"big-title"}>Become a Partner</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat
@@ -26,9 +19,7 @@ export default function PartnershipCta() {
           <button>Download an Application</button>
           <button>Contact Us</button>
         </div>
-
       </div>
-
     </section>
   );
 }
