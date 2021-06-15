58576996a912a1a4736faceb6185ff0b13bf5bd7
SVRourke <samuelvanrourke@gmail.com>
2021-06-15 11:20:50 -0400

    update titles

--- src/data/categoriesSlides.js
+++ src/data/categoriesSlides.js
@@ -1,36 +1,36 @@
 export const slides = [
     {
         tab: 'Video',
-        title: 'OLED, QLED, 4K & 8K',
+        title: 'Television & Home Theater',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Audio',
-        title: 'Speakers, Subwoofers & Soundbars',
+        title: 'Speakers & Audio Equipment',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
-        tab: 'Photography',
+        tab: 'Photo',
         title: 'Cameras, Lenses & Accessories',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
-        tab: 'Personal Electronics',
+        tab: 'Electronics',
         title: 'Gaming, Cellphones & Tablets',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
         tab: 'Drones',
-        title: 'Sed nisi felis, luctus quis molestie at',
+        title: 'FPV, Hobbyist & Professional',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
     },
     {
-        tab: 'Computers and I.T. Equipment',
+        tab: 'Computers & I.T. Equipment',
         title: 'Computers, Laptops & I.T. Equipment',
         blurb: "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
         img: 'https://images.pexels.com/photos/908430/pexels-photo-908430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
