9bfe90974afd477439a1ab6e130f9b0a8669f41f
SVRourke <samuelvanrourke@gmail.com>
2021-06-15 11:20:00 -0400

    added links to ids, formatting

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -1,51 +1,58 @@
-import React from 'react'
-import Logo from '../assets/top-logo.png'
-import Words from '../assets/trading-words.png'
-import '../styles/Header.scss'
-import BadgeText from '../components/BadgeText'
+import React from "react";
+import Logo from "../assets/top-logo.png";
+import Words from "../assets/trading-words.png";
+import "../styles/Header.scss";
+import BadgeText from "../components/BadgeText";
 export default function Header() {
-    return (
-        <header className={'block_full-screen'}>
+  return (
+    <header id="header" className={"block_full-screen"}>
+      <section className={"navbar"}>
+        <img src={Logo} />
 
-            <section className={'navbar'}>
-                <img src={Logo} />
-                
-                <nav>
-                    <a href="#">home</a>
-                    <a href="#">about</a>
-                    <a href="#">products</a>
-                    <a href="#">contact</a>
-                
-                </nav>
-            </section>
+        <nav>
+          <a href="#about">About</a>
+          <a href="#brands">Brands</a>
+          <a href="#categories">Products</a>
+          <a href="#partnerCta">Partner</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </section>
 
+      <section className={"header_content"}>
+        <div className={"column"}>
+          <img src={Words} />
 
-            <section className={'header_content'}>
-                <div className={'column'}>
-                    <img src={Words} />
-                    
-                    <div>
-                        <button>Learn More</button>
-                        <button>Partner With Us</button>
+          <div>
+            <a href="#about" className="header-buttons">
+              Learn More
+            </a>
+            <a href="#partnerCta" className="header-buttons">
+              Partner With Us
+            </a>
+          </div>
+        </div>
 
-                    </div>
+        <div className={"column"}>
+          <video
+            muted
+            autoPlay
+            loop
+            className="headerVid"
+            style={{ background: "black" }}
+          >
+            <source
+              src="https://ik.imagekit.io/ultimatec/video_b8MieOF-0.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </section>
 
-                </div>
-
-                <div className={'column'}>
-                    <video muted autoPlay loop className="headerVid" style={{background: 'black'}} > 
-                        <source src='https://ik.imagekit.io/ultimatec/video_b8MieOF-0.mp4' type="video/mp4" />
-                    </video>
-                </div>
-
-            </section>
-
-
-            <section className={'header_phone'}>
-                <a href="tel:9547830100">
-                    <BadgeText icon={'BsPhone'} text={'call 1+ 954-783-0100'}/>
-                </a>
-            </section>
-        </header>
-    )
+      <section className={"header_phone"}>
+        <a href="tel:9547830100">
+          <BadgeText icon={"BsPhone"} text={"call 1+ 954-783-0100"} />
+        </a>
+      </section>
+    </header>
+  );
 }
