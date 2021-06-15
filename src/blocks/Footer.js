d2a350cbfae2354831b373237043338757ad2005
SVRourke <samuelvanrourke@gmail.com>
2021-06-15 16:29:44 -0400

    add links to footer badges

--- src/blocks/Footer.js
+++ src/blocks/Footer.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import topLogo from '../assets/top-logo.png'
-import '../styles/Footer.scss'
-import BadgeText from '../components/BadgeText'
+import React from "react";
+import topLogo from "../assets/top-logo.png";
+import "../styles/Footer.scss";
+import BadgeText from "../components/BadgeText";
 
 export default function Footer() {
-    return (
-        <footer id='footer' className={'block'}>
-            <img src={topLogo} />
-            
-            <nav>
-                <a href="#header">Home</a>
-                <a href="#about">About</a>
-                <a href="#brands">Partners</a>
-            </nav>
-            
-            <nav>
-                <a href="#categories">Products</a>
-                <a href="#logistics">Logistics</a>
-                <a href="#partnerCta">Become A Distributor</a>
-            </nav>
-            
-            <div className='badges'>
-                <BadgeText icon={"BsMap"} text={"Hollywood FL, USA"} />
-                <BadgeText icon={"BsPhone"} text={"+1 954-783-0400"} />
-                <BadgeText icon={"BsEnvelopeOpen"} text={"send us a message!"} />
-            </div>
+  return (
+    <footer id="footer" className={"block"}>
+      <img src={topLogo} />
 
-        </footer>
-    )
+      <nav>
+        <a href="#header">Home</a>
+        <a href="#about">About</a>
+        <a href="#brands">Partners</a>
+      </nav>
+
+      <nav>
+        <a href="#categories">Products</a>
+        <a href="#logistics">Logistics</a>
+        <a href="#partnerCta">Become A Distributor</a>
+      </nav>
+
+      <div className="badges">
+        <BadgeText icon={"BsMap"} text={"Hollywood FL, USA"} />
+
+        <a href="tel:9547830100">
+          <BadgeText icon={"BsPhone"} text={"+1 954-783-0100"} />
+        </a>
+
+        <a href={"mailto:contact@tradingunlimited.com"}>
+          <BadgeText icon={"BsEnvelopeOpen"} text={"send us a message!"} />
+        </a>
+      </div>
+    </footer>
+  );
 }
