0f3e4a127180d06e4cb152c0d6291766eafa1753
SVRourke <samuelvanrourke@gmail.com>
2021-06-18 17:32:21 -0400

    add reveal animations

--- src/blocks/Header.js
+++ src/blocks/Header.js
@@ -3,56 +3,68 @@ import Logo from "../assets/top-logo.png";
 import Words from "../assets/trading-words.png";
 import "../styles/Header.scss";
 import BadgeText from "../components/BadgeText";
+import Fade from "react-reveal/Fade";
+
 export default function Header() {
   return (
-    <header id="header" className={"block_full-screen"}>
-      <section className={"navbar"}>
-        <img src={Logo} />
+    <header id="header" className={"block_full-screen snap-start"}>
+      <Fade down>
+        <section className={"navbar"}>
+          <img src={Logo} />
 
-        <nav>
-          <a href="#about">About</a>
-          <a href="#brands">Brands</a>
-          <a href="#categories">Products</a>
-          <a href="#partnerCta">Partner</a>
-          <a href="#contact">Contact</a>
-        </nav>
-      </section>
+          <nav>
+            <a href="#about">About</a>
+            <a href="#brands">Brands</a>
+            <a href="#categories">Products</a>
+            <a href="#partnerCta">Partner</a>
+            <a href="#contact">Contact</a>
+          </nav>
+        </section>
+      </Fade>
 
       <section className={"header_content"}>
-        <div className={"column"}>
-          <img src={Words} />
+        <Fade up delay={300} duration={900}>
+          <div className={"column"}>
+            <img src={Words} />
 
-          <div>
-            <a href="#about" className="header-buttons">
-              Learn More
-            </a>
-            <a href="#partnerCta" className="header-buttons">
-              Partner With Us
-            </a>
+            <Fade up delay={800} duration={1000}>
+              <div>
+                <a href="#about" className="header-buttons">
+                  Learn More
+                </a>
+                <a href="#partnerCta" className="header-buttons">
+                  Partner With Us
+                </a>
+              </div>
+            </Fade>
           </div>
-        </div>
+        </Fade>
 
-        <div className={"column"}>
-          <video
-            muted
-            autoPlay
-            loop
-            className="headerVid"
-            style={{ background: "black" }}
-          >
-            <source
-              src="https://ik.imagekit.io/ultimatec/video_b8MieOF-0.mp4"
-              type="video/mp4"
-            />
-          </video>
-        </div>
+        <Fade down delay={300} duration={900}>
+          <div className={"column"}>
+            <video
+              muted
+              autoPlay
+              loop
+              className="headerVid"
+              style={{ background: "black" }}
+            >
+              <source
+                src="https://ik.imagekit.io/ultimatec/video_b8MieOF-0.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </div>
+        </Fade>
       </section>
 
-      <section className={"header_phone"}>
-        <a href="tel:9547830100">
-          <BadgeText icon={"BsPhone"} text={"call 1+ 954-783-0100"} />
-        </a>
-      </section>
+      <Fade right>
+        <section className={"header_phone"}>
+          <a href="tel:9547830100">
+            <BadgeText icon={"BsPhone"} text={"call 1+ 954-783-0100"} />
+          </a>
+        </section>
+      </Fade>
     </header>
   );
 }
