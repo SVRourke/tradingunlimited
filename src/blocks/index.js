2274b48962d4e7867234d3e09e90d71ca7357209
SVRourke <samuelvanrourke@gmail.com>
2021-06-18 17:32:04 -0400

    add rightimage

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,19 +1,20 @@
-import Header from './Header'
-import About from './About'
-import Brands from './Brands'
-import ProductCategories from './ProductCategories'
-import Logistics from './Logistics'
-import PartnershipCta from './PartnershipCta'
-import Contact from './Contact'
-import Footer from './Footer'
-
+import Header from "./Header";
+import About from "./About";
+import Brands from "./Brands";
+import ProductCategories from "./ProductCategories";
+import Logistics from "./Logistics";
+import PartnershipCta from "./PartnershipCta";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import RightImage from "./RightImage";
 export {
-    Header,
-    About,
-    Brands,
-    ProductCategories,
-    Logistics,
-    PartnershipCta,
-    Contact,
-    Footer
-}
+  Header,
+  About,
+  Brands,
+  ProductCategories,
+  Logistics,
+  PartnershipCta,
+  Contact,
+  Footer,
+  RightImage,
+};
