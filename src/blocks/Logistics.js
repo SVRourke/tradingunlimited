12f1d906541545f43ff6acb1e739055ef81da1f5
SVRourke <samuelvanrourke@gmail.com>
2021-06-18 17:31:51 -0400

    format, add snap target

--- src/blocks/Logistics.js
+++ src/blocks/Logistics.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import '../styles/Logistics.scss'
-import { data } from '../data/logisticsData'
+import React from "react";
+import "../styles/Logistics.scss";
+import { data } from "../data/logisticsData";
 
-const LogisticsCard = ({data: {tab, title, content}}) => {
-    return (
-        <div className={'logistics-card'}>
-            <h4 className={'blue-tab'}>{tab}</h4>
-            <h2>{title}</h2>
-            <p>{content}</p>
-        </div>
-    )
-}
-
+const LogisticsCard = ({ data: { tab, title, content } }) => {
+  return (
+    <div className={"logistics-card"}>
+      <h4 className={"blue-tab"}>{tab}</h4>
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
+  );
+};
 
 export default function Logistics() {
-    const cards = data.map(d => <LogisticsCard data={d} />)
-    return (
-        <section id='logistics' className={'block_full-screen'}>
-            {cards}
-        </section>
-    )
+  const cards = data.map((d) => <LogisticsCard data={d} />);
+  return (
+    <section id="logistics" className={"block_full-screen snap-center"}>
+      {cards}
+    </section>
+  );
 }
