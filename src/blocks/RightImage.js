bd03d54c28aca5604e3d8e68f92e85f82cc7ab9d
SVRourke <samuelvanrourke@gmail.com>
2021-06-18 17:28:16 -0400

    create right image component

---
+++ src/blocks/RightImage.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+import "../styles/RightImage.scss";
+
+const conditionalStyle = (props) => {
+  return {
+    flexDirection: props ? "row-reverse" : "row",
+  };
+};
+
+export default function RightImage({ data: { title, blurb, img }, left }) {
+  const textStyles = {
+    textAlign: left ? "right" : "left",
+  };
+
+  return (
+    <section className="block-column rightImage snap-center">
+      <div className="row" style={conditionalStyle(left)}>
+        <Fade left={!left} right={left} delay={200} duration={300}>
+          <div className="column">
+            <img src={img} />
+          </div>
+        </Fade>
+
+        <Fade left={left} right={!left} delay={200} duration={300}>
+          <div className="block-column" style={textStyles}>
+            <h1 className={"block-title"}>{title}</h1>
+            <p>{blurb}</p>
+          </div>
+        </Fade>
+      </div>
+    </section>
+  );
+}
