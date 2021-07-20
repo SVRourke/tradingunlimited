a0e9c9e1a6b1f3998c285f3ffc740eea4b0f236e
SVRourke <samuelvanrourke@gmail.com>
2021-07-20 11:51:20 -0400

    add description to tv and hometheater card

--- src/data/categoriesSlides.js
+++ src/data/categoriesSlides.js
@@ -3,7 +3,7 @@ export const slides = [
     tab: "Video",
     title: "Television & \n\r Home Theater",
     blurb:
-      "est. Sed nisi felis, luctus quis molestie at, blandit ut velit. Aenean sit amet efficitur diam. Curabitur consequat justo in feugiat porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      "From Televisions to Surround-sound speakers and everything in between Our wide selection of Home Theater equipment is sure to have everything you need to craft a memorable theater experience in your home.",
     img: "https://ik.imagekit.io/ultimatec/video_q7WGmdY67.png",
   },
   {
