b0f4a19481ad33205051110c9ae4f6a0c3551938
SVRourke <samuelvanrourke@gmail.com>
2021-07-20 11:50:06 -0400

    create contact form component

---
+++ src/components/ContactForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "../styles/ContactForm.scss";
+
+export default function ContactForm() {
+  return (
+    <form>
+      <div className={"row"}>
+        <div className={"form-column"}>
+          <label for="name">Name:</label>
+          <input id="name" name="name" type="text" placeholder="so and so" />
+        </div>
+
+        <div className={"form-column"}>
+          <label for="email">Email:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="person@example.com"
+          />
+        </div>
+      </div>
+
+      <label for="message">Message:</label>
+      <textarea
+        id="message"
+        name="message"
+        type="message"
+        placeholder="enter a message..."
+      />
+
+      <input type="submit" value="Submit!" />
+    </form>
+  );
+}
