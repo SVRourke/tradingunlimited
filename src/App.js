8459d45d61673c2c127dce35708906880ea7adaf
SVRourke <samuelvanrourke@gmail.com>
2021-07-30 15:38:47 -0400

    switched to block category format

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React, { useEffect } from "react";
 import { slides } from "./data/categoriesSlides";
+import { categories } from "./data/categoryContent";
 import {
   Header,
   About,
   Brands,
   Logistics,
   PartnershipCta,
-  RightImage,
   Contact,
   Footer,
-  CategoryCarousel,
+  SideImageSlim,
+  MidImageFat,
 } from "./blocks";
 
 function App() {
-  const blocks = slides.map((s, idx) => {
-    const facingLeft = idx % 2 === 0 ? false : true;
-    return <RightImage data={s} left={facingLeft} />;
-  });
-
-  // useEffect(() => {
-  //   const header = document.querySelector("header");
-  //   header.focus();
-  // }, []);
+  useEffect(() => {
+    const header = document.querySelector("header");
+    header.focus();
+  }, []);
 
   return (
     <>
-      {/* <Header /> */}
-      {/* <About /> */}
-      {/* <Brands /> */}
-      <CategoryCarousel />
-      {blocks}
+      <Header />
+      <About />
+      <Brands />
+      <SideImageSlim data={categories["tv"]} right={false} />
+      <MidImageFat data={categories["photography"]} />
+      <SideImageSlim data={categories["audio"]} right={true} />
+      <MidImageFat data={categories["gadgets"]} />
+      <SideImageSlim data={categories["office"]} right={false} />
       <Logistics />
       <PartnershipCta />
       <Contact />
