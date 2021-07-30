21ccb203481b8e8ad7ba575f37388a1b0b88ba04
SVRourke <samuelvanrourke@gmail.com>
2021-07-30 15:39:55 -0400

    create category content file

---
+++ src/data/categoryContent.js
@@ -0,0 +1,37 @@
+export const categories = {
+  tv: {
+    title: "TV & Home Theater",
+    description:
+      "Consectetur elit do officia magna nulla exercitation esse Lorem. Exercitation cupidatat aliquip qui dolor tempor incididunt anim dolor aute non laborum veniam irure occaecat. Cillum aliquip do labore voluptate ea anim sint pariatur. Anim amet amet id nostrud id ad ut esse et. Pariatur dolor est esse velit dolor velit incididunt aliqua excepteur ad minim exercitation minim. Incididunt minim exercitation id ad ad ipsum id ex nisi qui occaecat. Consequat nostrud duis ad sit.",
+    img: "https://images.unsplash.com/flagged/photo-1572609239482-d3a83f976aa0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80",
+  },
+  audio: {
+    title: "Audio Equipment",
+    description:
+      "Consectetur elit do officia magna nulla exercitation esse Lorem. Exercitation cupidatat aliquip qui dolor tempor incididunt anim dolor aute non laborum veniam irure occaecat. Cillum aliquip do labore voluptate ea anim sint pariatur. Anim amet amet id nostrud id ad ut esse et. Pariatur dolor est esse velit dolor velit incididunt aliqua excepteur ad minim exercitation minim. Incididunt minim exercitation id ad ad ipsum id ex nisi qui occaecat. Consequat nostrud duis ad sit.",
+    img: "https://images.unsplash.com/photo-1490476323407-63a2b2baa393?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1369&q=80",
+  },
+
+  office: {
+    title: "Computers & I.T. Equipment",
+    description:
+      "Consectetur elit do officia magna nulla exercitation esse Lorem. Exercitation cupidatat aliquip qui dolor tempor incididunt anim dolor aute non laborum veniam irure occaecat. Cillum aliquip do labore voluptate ea anim sint pariatur. Anim amet amet id nostrud id ad ut esse et. Pariatur dolor est esse velit dolor velit incididunt aliqua excepteur ad minim exercitation minim. Incididunt minim exercitation id ad ad ipsum id ex nisi qui occaecat. Consequat nostrud duis ad sit.",
+    img: "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+  },
+  photography: {
+    title: "Cameras, Lenses & Photography Accessories",
+    description:
+      "Consectetur elit do officia magna nulla exercitation esse Lorem. Exercitation cupidatat aliquip qui dolor tempor incididunt anim dolor aute non laborum veniam irure occaecat. Cillum aliquip do labore voluptate ea anim sint pariatur. Anim amet amet id nostrud id ad ut esse et. Pariatur dolor est esse velit dolor velit incididunt aliqua excepteur ad minim exercitation minim. Incididunt minim exercitation id ad ad ipsum id ex nisi qui occaecat. Consequat nostrud duis ad sit.",
+    img1: "https://images.unsplash.com/photo-1492691527719-9d1e07e534b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80",
+    img2: "https://images.unsplash.com/photo-1520390138845-fd2d229dd553?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1489&q=80",
+    img3: "https://images.unsplash.com/photo-1596923322832-ada855ab1a61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  gadgets: {
+    title: "Personal Electronics",
+    description:
+      "Consectetur elit do officia magna nulla exercitation esse Lorem. Exercitation cupidatat aliquip qui dolor tempor incididunt anim dolor aute non laborum veniam irure occaecat. Cillum aliquip do labore voluptate ea anim sint pariatur. Anim amet amet id nostrud id ad ut esse et. Pariatur dolor est esse velit dolor velit incididunt aliqua excepteur ad minim exercitation minim. Incididunt minim exercitation id ad ad ipsum id ex nisi qui occaecat. Consequat nostrud duis ad sit.",
+    img1: "https://images.unsplash.com/photo-1547479117-da9abbff3fa0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    img2: "https://images.unsplash.com/photo-1606318801954-d46d46d3360a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    img3: "https://images.unsplash.com/photo-1550029402-8ea9bfe19f04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80",
+  },
+};
