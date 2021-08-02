fa6eeb281b0a9152ccb30c418f3961406ddc9b53
SVRourke <samuelvanrourke@gmail.com>
2021-08-02 15:59:33 -0400

    remove texts from brands component

--- src/blocks/Brands.js
+++ src/blocks/Brands.js
@@ -7,14 +7,6 @@ export default function Brands() {
   return (
     <Fade upin delay={400}>
       <section id="brands" className={"block_full-screen snap-center"}>
-        <h2 className={"big-title"}>Our Brand Partners</h2>
-        <p>
-          At Trading Unlimited we're proud to be authorized distributors of some
-          of the top brands in the industry. Whether you're looking for; a
-          high-speed lens for professional sports photography, a pair of
-          headphones or the latest 8K TVs Trading Unlimited has what you need.
-        </p>
-
         <Slider />
       </section>
     </Fade>
