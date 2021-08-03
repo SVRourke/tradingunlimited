28b3a5e36e52fd32a24f2d1f6f33cddfa4b1fb44
SVRourke <samuelvanrourke@gmail.com>
2021-08-03 15:56:42 -0400

    add flexibility for adding ids to the component

--- src/blocks/SideImageSlim.js
+++ src/blocks/SideImageSlim.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/components/_sideimageslim.scss";
 export default function SideImageSlim({
+  id,
   right,
-  data: { title, description, img },
+  data: { title, description, img, img2 },
 }) {
   const computedClassName = right ? "right-facing" : "left-facing";
 
   return (
-    <section className={`side-image-slim ${computedClassName}`}>
+    <section id={id || null} className={`side-image-slim ${computedClassName}`}>
       <div className="text-box">
         <h3>{title}</h3>
         <p>{description}</p>
@@ -16,6 +17,12 @@ export default function SideImageSlim({
         className="side-image"
         style={{
           backgroundImage: `url(${img})`,
+        }}
+      ></div>
+      <div
+        className="small-image"
+        style={{
+          backgroundImage: `url(${img2})`,
         }}
       ></div>
     </section>
